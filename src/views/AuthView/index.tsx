2518858aced2d28b16d6d78987470e31ed80919c
Anton Ivanov <anton.p.ivanov@gmail.com>
2021-12-27 20:24:01 +0300

    AuthView initial view

--- src/views/AuthView/index.tsx
+++ src/views/AuthView/index.tsx
@@ -7,14 +7,22 @@ import styles from './styles.module.scss';
 const AuthView: React.FC = () => (
   <div className={styles.view}>
     <div className={styles.form}>
-      Welcome to React.JS Application
-      <br />
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <img src={config.logo} alt={config.title} width="50" />
+        </div>
+        <div className={styles.title}>
+          {config.title}
+          <div className={styles.author}>
+            {`by ${config.author}`}
+          </div>
+        </div>
+      </div>
     </div>
     <div className={styles.copyright}>
       {config.copyright}
     </div>
   </div>
-
 );
 
 export default AuthView;
